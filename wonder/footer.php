243c703fd4af641fa27a45b524ae9215af8276d5
willbarnes <35780661+willbarnes@users.noreply.github.com>
2018-11-03 10:18:33 -0700

    Adds initial application files

---
+++ wonder/footer.php
@@ -0,0 +1,24 @@
+
+
+<!-- footer starts here -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="py-5 bg-black">
+      <div class="container">
+        <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2017-<?php echo date("Y"); ?></p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
